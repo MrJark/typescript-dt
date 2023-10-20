179a4901261f00b81711878578813c465558f11c
MrJark <mrjark7@gmail.com>
2023-10-20 07:19:36 +0200

    never

---
+++ bases/types/never.ts
@@ -0,0 +1,10 @@
+(() => {
+
+  // Una función never nunca puede acabar de forma satisfactoria, es decir, sirve para mandar errores porque lo más seguro es que no resuelvan la función de forma satisfactoria
+  const error = (msg: string):never => {
+    throw new Error (msg)
+  }
+  error('SOS')
+  
+  // never != null != undefined != void
+})()

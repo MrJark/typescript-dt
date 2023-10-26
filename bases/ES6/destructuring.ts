a3cd508a7fc726f2725456ebdd3dfaed7537c57a
MrJark <mrjark7@gmail.com>
2023-10-26 06:32:58 +0200

    destructuring to arrays

--- bases/ES6/destructuring.ts
+++ bases/ES6/destructuring.ts
@@ -19,7 +19,13 @@
   const printAven = ({ ironman, ...rest}:Avengers) => {
     console.log(ironman);
     console.log(rest);
-    
   }
+
+  // ------------------------
+  const avengersArr: string[] = ['Cap. America', 'Spiderman', 'Hulk']
+  // const ironman = avengersArr[1] // no se ve tan bonito como 👇🏼
+  const [, , Hulk] = avengersArr // -> del arreglo estás cogiendo el último y los espacios tambien pueden ser _ y no afecta
+  const [_, Spiderman] = avengersArr // -> los espacios tambien pueden ser _ y no afecta
+
 
 })()

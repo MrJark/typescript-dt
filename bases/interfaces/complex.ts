8975f003b10b7afa58024f119341db918957a7e4
MrJark <mrjark7@gmail.com>
2023-10-28 06:48:22 +0200

    interfaces complex

---
+++ bases/interfaces/complex.ts
@@ -0,0 +1,26 @@
+(() => {
+
+  // en las interfaces da igual la que vaya antes, aun si estás llamando una interfaz que aun no ha sido creada ya que la principal siempre tiene que ir arriba
+  interface Client { // si los elementos de las interfaces tienen más de un nivel, com es el address, es buena práctica hacer una nueva interface y añadir una nueva y mandarla a la principal
+    name: string,
+    age?: number,
+    address: Address
+  }
+
+  interface Address {
+    id: number,
+    zip: string,
+    city: string,
+  }
+
+  const client: Client = {
+    name: 'Chema',
+    age: 26,
+    address: {  
+      zip: '003301',
+      id: 345678,
+      city: 'Alicante'
+    }
+  }
+
+})()

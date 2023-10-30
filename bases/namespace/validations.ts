a9b51d85afd2ddf149eba6bf3ecdd22104db529f
MrJark <mrjark7@gmail.com>
2023-10-30 05:57:34 +0100

    namespaces explanation

--- bases/namespace/validations.ts
+++ bases/namespace/validations.ts
@@ -1,4 +1,17 @@
 
 namespace Validations {
-  const validateText()
+
+  // destro de un namespace puedes tener muchas clases dentro de el y se usan cunaco se crean librerías o frameworks
+
+  export const validateText = ( text: string ): boolean => {
+    return ( text.length > 3 ) ? true : false
+  }
+
+  export const validateDate = ( myDate: Date): boolean => {
+    return (isNaN(myDate.valueOf()))
+      ? true
+      : false
+  }
+  // solo si exportas las funciones se podrán usar fuera de este namespace
 }
+

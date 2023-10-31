72e124a9e1394bf572d617cb182fc339421c4744
MrJark <mrjark7@gmail.com>
2023-10-31 06:15:38 +0100

    task with get

--- ts_node/src/classes/Hero.ts
+++ ts_node/src/classes/Hero.ts
@@ -1,10 +1,25 @@
+import { powers } from "../data/powers";
 
 export class Hero {
   constructor(
     public name: string,
     public powerId: number,
-    public age: number
+    public age: number,
+    // public powers: [ // esto no era porque ya tenia el arreglo de powers en la data
+    //   {
+    //     id: 1,
+    //     desc: 'Super'
+    //   },
+    //   {
+    //     id: 2,
+    //     desc: 'Super2'
+    //   },
+    // ]
   ){}
+
+  get power(): string {
+    return powers.find( power => power.id === this.powerId)?.description || 'not found'
+  }
 }
 
 export class Her2 {

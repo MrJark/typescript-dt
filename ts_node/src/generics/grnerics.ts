e0373a5f0cd1efb246bf11ad73bfe8ae4b85a59d
MrJark <mrjark7@gmail.com>
2023-10-31 06:42:24 +0100

    generic functions

---
+++ ts_node/src/generics/grnerics.ts
@@ -0,0 +1,16 @@
+
+
+export const printObject = ( args: any ) => {
+  console.log(args); 
+}
+
+// Función normal 👇🏼
+// export function genericFuntion (args: any ) {
+//   return args
+// }
+// Función genérica con function 👇🏼 ( LA T LE INDICA QUE ES TIPO GENÉRICO )
+export function genericFuntion<T> (args: T ) {
+  return args
+}
+
+export const genericFuntionArrow = <T> (args: T) => args // est ay la de arriba son la misma pero en arrow
--- ts_node/src/index.ts
+++ ts_node/src/index.ts
@@ -1,13 +1,24 @@
-import { Hero } from "./classes/Hero";
-// se pueden hacer alias en los imports 👇🏼
-import { Hero as Patatas } from "./classes/Hero";
-// también se pueden hacer importaciones por defecto 👇🏼
-import Hero3 from "./classes/Hero";
-// también se puede exportar todo y usar el inteligence 👇🏼
-import * as TDOD from "./classes/Hero"
+// import { Hero } from "./classes/Hero";
+// // se pueden hacer alias en los imports 👇🏼
+// import { Hero as Patatas } from "./classes/Hero";
+// // también se pueden hacer importaciones por defecto 👇🏼
+// import Hero3 from "./classes/Hero";
+// // también se puede exportar todo y usar el inteligence 👇🏼
+// import * as TDOD from "./classes/Hero"
 
-const ironman = new Hero('Ironman', 1, 55)
-const ironman2 = new Patatas('Ironman', 1, 55)
-const ironman3 = new TDOD.Her4('Ironman', 1, 55)
+
+// const ironman = new Hero('Ironman', 1, 55)
+// const ironman2 = new Patatas('Ironman', 1, 55)
+// const ironman3 = new TDOD.Her4('Ironman', 1, 55)
 
 //_________________
+
+import { printObject, genericFuntion } from "./generics/grnerics";
+
+printObject( 123 )
+printObject( '123' )
+printObject( true )
+printObject( Date() )
+
+console.log( genericFuntion(3.1416).toFixed(3) )
+// console.log( genericFuntion('Hola mundo').toFixed(2) ) // gracias a ponerle el tipo genérico T esto me da error sino, no me da

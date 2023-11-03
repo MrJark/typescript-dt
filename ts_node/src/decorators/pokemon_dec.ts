5a7a8ec2dd31e31b1d056a3617dd9831b51f7a6b
MrJark <mrjark7@gmail.com>
2023-11-03 06:51:52 +0100

    factory and block decorators

--- ts_node/src/decorators/pokemon_dec.ts
+++ ts_node/src/decorators/pokemon_dec.ts
@@ -3,11 +3,60 @@ function printToConsole( constructor: Function ){
   console.log(constructor);
 }
 
-@printToConsole // aplicando el decorador ( justo antes de las classes con el @ antes) y los abilito en el tsconfig
+// factory decorator 
+const printToConsoleCondifional = ( print: boolean = false): Function => {
+  // return () => console.log('Hola mundo');
+  if( print ) {
+    return printToConsole
+  } else {
+    return () => {}
+  }
+}
+
+// block portotype ( evita que alguien haga expansiones de ciertas propiedades, en este caso del constructor)
+const blockPrototype = function(constructor: Function){
+  Object.seal(constructor)
+  Object.seal(constructor.prototype)
+}
+
+// @printToConsole // aplicando el decorador ( justo antes de las classes con el @ antes) y los abilito en el tsconfig
+// export class Pokemon {
+//   public publicApi: string = 'https://pokeapi.co'
+
+//   constructor(
+//     public name: string
+//   ){}
+// }
+
+// @printToConsoleCondifional()
+// export class Pokemon {
+//   public publicApi: string = 'https://pokeapi.co'
+
+//   constructor(
+//     public name: string
+//   ){}
+// }
+
+
+function CheckValidPokemonId() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDecorator){
+    console.log({target, propertyKey, descriptor });
+    
+  }
+}
+
+// puedo colocar tantos decoradores como quiera y se ejecutan de forma secuencial en transpolacion/compolación
+@blockPrototype
+@printToConsoleCondifional()
 export class Pokemon {
   public publicApi: string = 'https://pokeapi.co'
 
   constructor(
     public name: string
   ){}
+
+  savePokemonToDB( id:number) {
+    console.log(`El Pokemon con el id ${id} se ha guardado`);
+    
+  }
 }
